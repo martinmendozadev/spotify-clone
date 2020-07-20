e39a94067ec84e3f9e7527d28b947e2086806fb1
Yair Mendoza <yairmendo@gmail.com>
2020-07-19 20:11:50 -0500

    Ajuses al manejo de strings

--- spotify/scraping/scraping.py
+++ spotify/scraping/scraping.py
@@ -1,13 +1,6 @@
+
 import requests
 
-url_base = 'https://api.spotify.com/v1'
-
-ep_artist = '/artists/{artist_id}'
-id_zedd = '2qxJFvFYMEDqd7ui6kSAcq'
-artist_id = id_zedd
-
-#r = requests.get(url_base+ep_artist.format(artist_id = id_zedd))
-#print(r.status_code)
 
 def get_token():
 
@@ -16,27 +9,24 @@ def get_token():
     headers = {'Authorization': 'Basic ODAwMjM3YWRhMWI4NGUxYTlmNTU1YjY3OGIxZGM5Y2I6ZmQxNjA0Y2Q2YzM4NGIzYThlYzA4ZjI4ZDM0NzA2MWU'}
     r = requests.post(token_url, data = params, headers = headers )
 
-    print(r.status_code)
-    print(r.json())
-    token = r.json()['access_token']
-    return print(r.status_code)
+    return r.json()['access_token']
 
 
 def get_info_by_artist_id(artist_id, token):
+    ep_artist = '/artists/{artist_id}'
+    url_base = 'https://api.spotify.com/v1'
     header = {'Authorization': 'Bearer {}'.format(token)}
     r = requests.get(url_base+ep_artist.format(artist_id = artist_id), headers = header)
-
-    print(r.status_code)
-
-    print(r.json())
     info_by_artist = r.json()
 
     return info_by_artist
 
 
-def get_info_by_artist_name(artist_name):
+def get_info_by_artist_name(artist_name, token):
+    artist_name = artist_name.replace(' ', '+')
     url_search = 'https://api.spotify.com/v1/search'
-    search_params = {'q': artist_name, 'type':'artist', 'market':'MX'}
+    search_params = {'q': artist_name, 'type':'artist', 'market':'MX', 'limit':1}
+    header = {'Authorization': 'Bearer {}'.format(token)}
     search = requests.get(url_search, headers = header, params = search_params)
 
     print(search.status_code)
@@ -45,9 +35,11 @@ def get_info_by_artist_name(artist_name):
     return result_search_artist_name
 
 
-def get_info_by_album_name(album_name):
+def get_info_by_album_name(album_name, token):
+    album_name = album_name.replace(' ', '+')
     url_search = 'https://api.spotify.com/v1/search'
-    search_params = {'q': album_name, 'type':'album', 'market':'MX'}
+    search_params = {'q': album_name, 'type':'album', 'market':'MX', 'limit':1}
+    header = {'Authorization': 'Bearer {}'.format(token)}
     search = requests.get(url_search, headers = header, params = search_params)
 
     print(search.status_code)
@@ -56,9 +48,11 @@ def get_info_by_album_name(album_name):
     return result_search_album_name
 
 
-def get_info_by_track_name(artist_name):
+def get_info_by_track_name(track_name, token):
+    track_name = track_name.replace(' ', '+')
     url_search = 'https://api.spotify.com/v1/search'
-    search_params = {'q': track_name, 'type':'track', 'market':'MX'}
+    search_params = {'q': track_name, 'type':'track', 'market':'MX', 'limit':1}
+    header = {'Authorization': 'Bearer {}'.format(token)}
     search = requests.get(url_search, headers = header, params = search_params)
 
     print(search.status_code)
@@ -67,9 +61,11 @@ def get_info_by_track_name(artist_name):
     return result_search_track_name
 
 
-def get_info_by_playlist_name(artist_name):
+def get_info_by_playlist_name(playlist_name, token):
+    playlist_name = playlist_name.replace(' ', '+')
     url_search = 'https://api.spotify.com/v1/search'
-    search_params = {'q': playlist_name, 'type':'playlist', 'market':'MX'}
+    search_params = {'q': playlist_name, 'type':'playlist', 'market':'MX', 'limit':1}
+    header = {'Authorization': 'Bearer {}'.format(token)}
     search = requests.get(url_search, headers = header, params = search_params)
 
     print(search.status_code)
@@ -78,9 +74,11 @@ def get_info_by_playlist_name(artist_name):
     return result_search_playlist_name
 
 
-def get_info_by_show_name(artist_name):
+def get_info_by_show_name(show_name, token):
+    show_name = show_name.replace(' ', '+')
     url_search = 'https://api.spotify.com/v1/search'
-    search_params = {'q': show_name, 'type':'show', 'market':'MX'}
+    search_params = {'q': show_name, 'type':'show', 'market':'MX', 'limit':1}
+    header = {'Authorization': 'Bearer {}'.format(token)}
     search = requests.get(url_search, headers = header, params = search_params)
 
     print(search.status_code)
@@ -89,29 +87,16 @@ def get_info_by_show_name(artist_name):
     return result_search_show_name
 
 
-def run():
-    token_url = 'https://accounts.spotify.com/api/token'
-    params = {'grant_type': 'client_credentials'}
-    headers = {'Authorization': 'Basic ODAwMjM3YWRhMWI4NGUxYTlmNTU1YjY3OGIxZGM5Y2I6ZmQxNjA0Y2Q2YzM4NGIzYThlYzA4ZjI4ZDM0NzA2MWU'}
-    r = requests.post(token_url, data = params, headers = headers )
-
-    print(r.status_code)
-    print(r.json())
-    token = r.json()['access_token']
-
-    print('step2 ok')
-
-    header = {'Authorization': 'Bearer {}'.format(token)}
-    r = requests.get(url_base+ep_artist.format(artist_id = artist_id), headers = header)
-
-    print(r.status_code)
-
-    print(r.json())
-    info_by_artist = r.json()
-
-
-
 if __name__ == '__main__':
 
-    run()
-
+    token = get_token()
+    artist_name = input('Que artista buscas? ')
+    print(get_info_by_artist_name(artist_name, token))
+    """ track_name = input('Que cancion buscas? ').replace(' ', '+')
+    print(get_info_by_track_name(track_name, token))
+    album_name = input('Que album buscas? ').replace(' ', '+')
+    print(get_info_by_album_name(album_name, token))
+    playlist_name = input('Que playlist buscas? ').replace(' ', '+')
+    print(get_info_by_playlist_name(playlist_name, token))
+    show_name = input('Que show buscas? ').replace(' ', '+')
+    print(get_info_by_show_name(show_name, token)) """
