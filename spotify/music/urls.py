cf021633576bb7303e7f5fadfad67e075fcd049e
Martin Mendoza <marttcode@gmail.com>
2020-07-24 00:58:02 -0500

    views required

--- spotify/music/urls.py
+++ spotify/music/urls.py
@@ -10,8 +10,23 @@ from spotify.music import views
 urlpatterns = [
     # Management
     path(
-        route='mylist/',
+        route='search/',
         view=views.search_music,
-        name='myList'
+        name='search'
+    ),
+    path(
+        route='my-favorites/',
+        view=views.favorite_music,
+        name='myFavorite'
+    ),
+    path(
+        route='playMusic/',
+        view=views.play_music,
+        name='playMusic'
+    ),
+    path(
+        route='detailMusic/',
+        view=views.detali_music,
+        name='detailMusic'
     ),
 ]
--- spotify/music/views.py
+++ spotify/music/views.py
@@ -1,16 +1,36 @@
+"""Music views."""
+
+# Django
+from django.shortcuts import render
 
 
-def search_music():
-    pass
+def search_music(request):
+    return render(
+        request,
+        'music/search.html',
+    )
 
-def detali_music():
-    pass
 
-def paly_music():
-    pass
+def detali_music(request):
+    return render(
+        request,
+        'music/detail.html',
+    )
+
+
+def play_music(request):
+    return render(
+        request,
+        'music/play.html',
+    )
+
 
 def save_music():
     pass
 
-def favorite_music():
-    pass
+
+def favorite_music(request):
+    return render(
+        request,
+        'music/favorites.html',
+    )
